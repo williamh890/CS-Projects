d3fa726b72850147bf0d5866f37c5a5e8eea4841
William Horn <wbhorn@alaska.edu>
2016-11-05 01:09:55 -0800

    Added Level prompt screens and updated enemy movement

---
+++ bullets/bullets/readLevel.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <string>
+using std::string;
+#include <fstream>
+using std::ifstream;
+#include "readLevel.h"
+
+
+
+string readLevel(int num)
+{
+    string line;
+    string currLevel = "\n\n\n\n\n";
+    string levelNum;
+    switch(num)
+    {
+    case 1:
+        levelNum = "1";
+        break;
+    case 2:
+        levelNum = "2";
+        break;
+    case 3:
+        levelNum = "3";
+        break;
+    case 4:
+        levelNum = "4";
+        break;
+    case 5:
+        levelNum = "5";
+        break;
+    case 6:
+        levelNum = "6";
+        break;
+    case 7:
+        levelNum = "7";
+        break;
+    case 8:
+        levelNum = "8";
+        break;
+    case 9:
+        levelNum = "9";
+        break;
+    case 10:
+        levelNum = "10";
+        break;
+    }
+
+    string levelFile = "Levels/level" + levelNum + ".txt";
+    ifstream myfile (levelFile);
+
+    if (myfile.is_open())
+    {
+        while ( getline (myfile,line) )
+        {
+            currLevel += "\t\t" + line + "\n";
+        }
+        myfile.close();
+    }
+    else cout << "Unable to open file";
+
+    return currLevel;
+}
+
---
+++ bullets/bullets/readLevel.h
@@ -0,0 +1,13 @@
+#ifndef READLEVEL_H_INCLUDED
+#define READLEVEL_H_INCLUDED
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <string>
+using std::string;
+#include <fstream>
+using std::ifstream;
+
+string readLevel(int);
+
+#endif // READLEVEL_H_INCLUDED
