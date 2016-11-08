04fbd1fb75029c6eccfe75e5ecb3e0b932638dcf
williamh890 <williamh890@yahoo.com>
2016-11-07 16:47:47 -0900

    working matrix += and consolidated constructor

--- Matrix/MatrixClass/include/Matrix.h
+++ Matrix/MatrixClass/include/Matrix.h
@@ -15,12 +15,12 @@ class Matrix
     public:
         //Zero by default
         Matrix(int r, int c);
-        Matrix(int r, int c, T val);
+        Matrix(int r, int c, T val = 0);
         Matrix(std::vector<std::vector<T> > data);
 
         ~Matrix();
         //Operations
-        Matrix& operator+(const Matrix& b);
+        Matrix& operator+=(const Matrix& b);
 
         //Display
         void print();
@@ -31,17 +31,7 @@ class Matrix
 };
 
 template <typename T>
-Matrix<T>::Matrix(int r, int c) : row(r), col(c)
-{
-    matrixVals.resize(r);
-    for(int i = 0; i < matrixVals.size(); ++i)
-    {
-        matrixVals[i].resize(c, 0);
-    }
-}
-
-template <typename T>
-Matrix<T>::Matrix(int r, int c, T val)
+Matrix<T>::Matrix(int r, int c, T val) :  row(r), col(c)
 {
     matrixVals.resize(r);
     for(int i = 0; i < matrixVals.size(); ++i)
@@ -58,17 +48,16 @@ Matrix<T>::Matrix(std::vector<std::vector<T> > data) : matrixVals(data)
 }
 
 template <typename T>
-Matrix<T>& Matrix<T>::operator+(const Matrix & b)
+Matrix<T>& Matrix<T>::operator+=(const Matrix & b)
 {
-    Matrix matrixSum(this->row, this->col);
     for(int r = 0; r < this->row; ++r)
     {
         for(int c = 0; c < this->col; ++c)
         {
-            matrixSum.matrixVals[r][c] = this->matrixVals[r][c] + b.matrixVals[r][c];
+            this->matrixVals[r][c] += b.matrixVals[r][c];
         }
     }
-    return matrixSum;
+    return *this;
 }
 
 template <typename T>
--- Matrix/MatrixClass/main.cpp
+++ Matrix/MatrixClass/main.cpp
@@ -10,7 +10,9 @@ int main()
     Matrix<int> first(2,3,3);
     Matrix<int> second(2,3,4);
 
-    first = first + second;
+    first += second;
+
+    first.print();
 
     return 0;
 }
